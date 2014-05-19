2e2afc594bbdf50cd5327699dca283ebe098024b
Christophe Cleva <christophe.cleva@esrf.fr>
2014-05-19 14:18:48 +0200

    Connection testing now tests the ISIS and the ESRF ICATs.

--- client-api/src/test/java/fr/esrf/icat/test/TestConnection.java
+++ client-api/src/test/java/fr/esrf/icat/test/TestConnection.java
@@ -1,28 +1,5 @@
 package fr.esrf.icat.test;
 
-/*
- * #%L
- * ICAT client API
- * %%
- * Copyright (C) 2014 ESRF - The European Synchrotron
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
-
 import com.btr.proxy.search.ProxySearch;
 
 import java.io.BufferedReader;
@@ -39,10 +16,12 @@ import javax.net.ssl.SSLPeerUnverifiedException;
 
 public class TestConnection {
 
-	private final static String ICAT_SERVER = "https://icatisis.esc.rl.ac.uk";
+	private final static String ICAT_ISIS_SERVER = "https://icatisis.esc.rl.ac.uk";
 	
+	private final static String ICAT_ESRF_SERVER = "https://wwws.esrf.fr/icat/";
+
 	private final static String ICAT_WSDL = "ICATService/ICAT?wsdl";  
-	
+
 	private final static Logger LOG = Logger.getLogger(TestConnection.class.getName());
 	
 	public static void main(String[] args) {
@@ -66,9 +45,17 @@ public class TestConnection {
         
 		ProxySelector.setDefault(myProxySelector);
 		
+		testServer(ICAT_ISIS_SERVER);
+		
+		testServer(ICAT_ESRF_SERVER);
+	}
+
+	private static void testServer(final String serverURLString) {
+		System.out.println();
+		System.out.println("****** Connecting to " + serverURLString + " ********");			
 		URL serverURL;
 		try {
-			serverURL = new URL(ICAT_SERVER);
+			serverURL = new URL(serverURLString);
 			URL wsdlURL = new URL(serverURL, ICAT_WSDL);
 		
 			HttpsURLConnection con = (HttpsURLConnection) wsdlURL.openConnection();
@@ -80,58 +67,57 @@ public class TestConnection {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 	   private static void print_https_cert(HttpsURLConnection con){
 		   
 		    if(con!=null){
 		 
-		      try {
+			    try {
+					System.out.println("****** SSL parameters ********");			
+					System.out.println("Response Code : " + con.getResponseCode());
+					System.out.println("Cipher Suite : " + con.getCipherSuite());
+					System.out.println("\n");
+					 
+					Certificate[] certs = con.getServerCertificates();
+					for(Certificate cert : certs){
+					   System.out.println("Cert Type : " + cert.getType());
+					   System.out.println("Cert Hash Code : " + cert.hashCode());
+					   System.out.println("Cert Public Key Algorithm : " + cert.getPublicKey().getAlgorithm());
+					   System.out.println("Cert Public Key Format : " + cert.getPublicKey().getFormat());
+					   System.out.println("\n");
+					}
+					 
+					} catch (SSLPeerUnverifiedException e) {
+						e.printStackTrace();
+					} catch (IOException e){
+						e.printStackTrace();
+					}
 		 
-			System.out.println("Response Code : " + con.getResponseCode());
-			System.out.println("Cipher Suite : " + con.getCipherSuite());
-			System.out.println("\n");
+		    }
 		 
-			Certificate[] certs = con.getServerCertificates();
-			for(Certificate cert : certs){
-			   System.out.println("Cert Type : " + cert.getType());
-			   System.out.println("Cert Hash Code : " + cert.hashCode());
-			   System.out.println("Cert Public Key Algorithm : " + cert.getPublicKey().getAlgorithm());
-			   System.out.println("Cert Public Key Format : " + cert.getPublicKey().getFormat());
-			   System.out.println("\n");
-			}
+	   }
 		 
-			} catch (SSLPeerUnverifiedException e) {
-				e.printStackTrace();
-			} catch (IOException e){
-				e.printStackTrace();
-			}
-		 
-		     }
-		 
-		   }
-		 
-		   private static void print_content(HttpsURLConnection con){
+	   private static void print_content(HttpsURLConnection con){
 			if(con!=null){
 		 
-			try {
+				try {
+			 
+				   System.out.println("****** Content ********");			
+				   BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			 
+				   String input;
+			 
+				   while ((input = br.readLine()) != null){
+				      System.out.println(input);
+				   }
+				   br.close();
+			 
+				} catch (IOException e) {
+				   e.printStackTrace();
+				}
 		 
-			   System.out.println("****** Content of the URL ********");			
-			   BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		 
-			   String input;
-		 
-			   while ((input = br.readLine()) != null){
-			      System.out.println(input);
-			   }
-			   br.close();
-		 
-			} catch (IOException e) {
-			   e.printStackTrace();
 			}
-		 
-	       }
 		 
 	   }
 }
